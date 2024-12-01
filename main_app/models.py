99b3fc41761c4ab6f3c4afe8ed90fd1b845bf289
tntyousif <yousif02bh@gmail.com>
2024-12-01 21:50:06 +0300

    :

---
+++ main_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField("Feeding Time")
+    meal = models.CharField(
+            max_length=1,
+            choices=MEALS,
+            default=MEALS[0][0]
+        )
+
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
---
+++ main_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+
+# CBVs
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
